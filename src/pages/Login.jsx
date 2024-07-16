025bfa561b1025b048babae6e627397cadc09d1d
Nisha0202 <nishajabatunnessa@gmail.com>
2024-07-17 00:22:56 +0600

    usertoken stored

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link, useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
   const [emailOrMobile, setEmailOrMobile] = useState('');
   const [pin, setPin] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('http://localhost:5000/api/login', {
         emailOrMobile,
-        pin
+        pin,
       });
+
       // Handle successful login
       console.log('Login successful:', response.data);
-      navigate('/overview'),
-      setError('');
-      // Save JWT token to localStorage or session storage
+      const token = response.data.token;
+      localStorage.setItem('usertoken', token); // Store token in localStorage
+      setError('');;
+
+      // Navigate to '/overview'
+      navigate('/overview');
     } catch (error) {
       setError('Login failed. Please check your credentials.');
       console.error('Login error:', error);
@@ -26,8 +32,8 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen-100 flex items-center justify-center bg-gray-100">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded p-6 mb-4 w-full max-w-md m-2">
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded p-6 mb-4 w-full max-w-md">
         <h2 className="text-xl font-bold mb-6 text-center">Login</h2>
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <div className="mb-4">
@@ -56,10 +62,7 @@ const Login = () => {
             required
           />
         </div>
-        <div className='flex items-center gap-4 justify-between'>
-          <Link to={'/'} className="btn border-blue-500 flex-1 border-2 text-gray-600 py-2 px-4 rounded">
-            Back
-          </Link>
+        <div className="flex items-center gap-4 justify-between">
           <button type="submit" className="btn bg-blue-500 flex-1 font-bold text-white py-2 px-4 rounded">
             Log In
           </button>
@@ -67,7 +70,6 @@ const Login = () => {
       </form>
     </div>
   );
-  
 };
 
 export default Login;
